c4668369d68f0f7e6339cf689dad84f094257706
DannyDeng <1041422509@qq.com>
2023-07-04 13:01:26 +0800

    Add Shadow Loader. Existing bug: shadow mem not release

--- mixture.py
+++ mixture.py
@@ -18,7 +18,7 @@ from dgl.data import AsNodePredDataset
 from dgl.dataloading import (
     DataLoader,
     MultiLayerFullNeighborSampler,
-    NeighborSampler,
+    NeighborSampler, ShaDowKHopSampler,
 )
 from ogb.nodeproppred import DglNodePropPredDataset
 from torch.profiler import profile, record_function, ProfilerActivity
@@ -29,6 +29,11 @@ import torch.multiprocessing as mp
 import dgl.multiprocessing as dmp
 from torch.nn.parallel import DistributedDataParallel
 import psutil
+
+from merge import merge_trace_files
+
+TRACE_NAME = 'mixture_product_{}.json'
+OUTPUT_TRACE_NAME = "combine.json"
 
 
 class SAGE(nn.Module):
@@ -45,52 +50,19 @@ class SAGE(nn.Module):
 
     def forward(self, blocks, x):
         h = x
-        for l, (layer, block) in enumerate(zip(self.layers, blocks)):
-            h = layer(block, h)
-            if l != len(self.layers) - 1:
-                h = F.relu(h)
-                h = self.dropout(h)
-        return h
-
-    def inference(self, g, device, batch_size):
-        """
-        Conduct layer-wise inference to get all the node embeddings.
-        TODO: Should be compatible with UVA
-        https://github.com/dmlc/dgl/blob/master/examples/pytorch/multigpu/multi_gpu_node_classification.py
-        """
-        feat = g.ndata["feat"]
-        sampler = MultiLayerFullNeighborSampler(1, prefetch_node_feats=["feat"])
-        dataloader = DataLoader(
-            g,
-            torch.arange(g.num_nodes()).to(g.device),
-            sampler,
-            device=device,
-            batch_size=batch_size,
-            shuffle=False,
-            drop_last=False,
-            num_workers=3
-        )
-        buffer_device = torch.device("cpu")
-        pin_memory = buffer_device != device
-
-        for l, layer in enumerate(self.layers):
-            y = torch.empty(
-                g.num_nodes(),
-                self.hid_size if l != len(self.layers) - 1 else self.out_size,
-                device=buffer_device,
-                pin_memory=pin_memory,
-            )
-            feat = feat.to(device)
-            for input_nodes, output_nodes, blocks in tqdm(dataloader):
-                x = feat[input_nodes]
-                h = layer(blocks[0], x)  # len(blocks) = 1
+        if hasattr(blocks, '__len__'):
+            for l, (layer, block) in enumerate(zip(self.layers, blocks)):
+                h = layer(block, h)
                 if l != len(self.layers) - 1:
                     h = F.relu(h)
                     h = self.dropout(h)
-                # by design, our output nodes are contiguous
-                y[output_nodes[0]: output_nodes[-1] + 1] = h.to(buffer_device)
-            feat = y
-        return y
+        else:
+            for l, layer in enumerate(self.layers):
+                h = layer(blocks, h)
+                if l != len(self.layers) - 1:
+                    h = F.relu(h)
+                    h = self.dropout(h)
+        return h
 
 
 class UnevenDDPTensorizedDataset(torch.utils.data.IterableDataset):
@@ -100,6 +72,7 @@ class UnevenDDPTensorizedDataset(torch.utils.data.IterableDataset):
     This class additionally saves the index tensor in shared memory and therefore
     avoids duplicating the same index tensor during shuffling.
     """
+
     def __init__(self, indices, total_batch_size, sub_batch_sizes, drop_last, shuffle):
         self.rank = dist.get_rank()
         self.seed = 0
@@ -161,31 +134,6 @@ class UnevenDDPTensorizedDataset(torch.utils.data.IterableDataset):
         return self.total_size
 
 
-def evaluate(model, graph, dataloader, load_core, comp_core):
-    model.eval()
-    ys = []
-    y_hats = []
-    with dataloader.enable_cpu_affinity(loader_cores=load_core, compute_cores=comp_core):
-        for it, (input_nodes, output_nodes, blocks) in enumerate(dataloader):
-            with torch.no_grad():
-                x = blocks[0].srcdata["feat"]
-                ys.append(blocks[-1].dstdata["label"])
-                y_hats.append(model(blocks, x))
-                accuracy = MulticlassAccuracy(num_classes=47)
-    return accuracy(torch.cat(y_hats), torch.cat(ys))
-
-
-def layerwise_infer(device, graph, nid, model, batch_size):
-    model.eval()
-    with torch.no_grad():
-        pred = model.inference(
-            graph, device, batch_size
-        )  # pred in buffer_device
-        pred = pred[nid]
-        label = graph.ndata["label"][nid].to(pred.device)
-        return MF.accuracy(pred, label)
-
-
 def is_cpu_proc(num_cpu_proc, rank=None):
     if rank is None:
         rank = dist.get_rank()
@@ -203,8 +151,8 @@ def assign_cores(num_cpu_proc):
     load_core, comp_core = [], []
     n = psutil.cpu_count(logical=False)
     if num_cpu_proc == 1:
-        load_core = list(range(0, 4))
-        comp_core = list(range(4, n))
+        load_core = list(range(0, 8))
+        comp_core = list(range(8, n))
     elif num_cpu_proc == 2:
         if rank == 0:
             load_core = list(range(0, 4))
@@ -214,17 +162,17 @@ def assign_cores(num_cpu_proc):
             comp_core = list(range(n // 2 + 4, n))
     elif num_cpu_proc == 4:
         if rank == 0:
-            load_core = list(range(0, 4))
-            comp_core = list(range(4, n // 4))
+            load_core = list(range(0, 2))
+            comp_core = list(range(2, n // 4))
         elif rank == 1:
-            load_core = list(range(n // 4, n // 4 + 4))
-            comp_core = list(range(n // 4 + 4, n // 2))
+            load_core = list(range(n // 4, n // 4 + 2))
+            comp_core = list(range(n // 4 + 2, n // 2))
         elif rank == 2:
-            load_core = list(range(n // 2, n // 2 + 4))
-            comp_core = list(range(n // 2 + 4, n // 4 * 3))
+            load_core = list(range(n // 2, n // 2 + 2))
+            comp_core = list(range(n // 2 + 2, n // 4 * 3))
         else:
-            load_core = list(range(n // 4 * 3, n // 4 * 3 + 4))
-            comp_core = list(range(n // 4 * 3 + 4, n))
+            load_core = list(range(n // 4 * 3, n // 4 * 3 + 2))
+            comp_core = list(range(n // 4 * 3 + 2, n))
     return load_core, comp_core
 
 
@@ -248,14 +196,21 @@ def _train(loader, model, opt, **kwargs):
         epoch = kwargs['epoch']
         pbar.set_description(f'Epoch {epoch:02d}')
     for it, (input_nodes, output_nodes, blocks) in enumerate(loader):
-        x = blocks[0].srcdata["feat"]
-        y = blocks[-1].dstdata["label"]
-        y_hat = model(blocks, x)
-        loss = F.cross_entropy(y_hat, y)
+        if hasattr(blocks, '__len__'):
+            x = blocks[0].srcdata["feat"]
+            y = blocks[-1].dstdata["label"]
+            y_hat = model(blocks, x)
+            loss = F.cross_entropy(y_hat, y)
+        else:
+            x = blocks.srcdata["feat"]
+            y = blocks.dstdata["label"]
+            y_hat = model(blocks, x)
+            loss = F.cross_entropy(y_hat[:output_nodes.shape[0]],
+                                   y[:output_nodes.shape[0]])
         opt.zero_grad()
         loss.backward()
         opt.step()
-        total_loss += loss.item()
+        total_loss += loss
         if kwargs['rank'] == 0:
             pbar.update(kwargs['batch_size'])
             # pbar.update(output_nodes.shape[0])
@@ -288,16 +243,21 @@ def train(rank, world_size, args, g, data):
     sub_batch_sizes = [get_subbatch_size(args, r) for r in range(world_size)]
     if rank == 0: print('SubBatch sizes:', sub_batch_sizes)
     train_indices = UnevenDDPTensorizedDataset(
-        train_idx.to(device),
+        # train_idx.to(device),
+        test_idx.to(device),
         args.batch_size,
         sub_batch_sizes,
         drop_last,
         shuffle
     )
-    sampler = NeighborSampler(
-        [15, 10, 5],
+    # sampler = NeighborSampler(
+    #     [15, 10, 5],
+    #     prefetch_node_feats=["feat"],
+    #     prefetch_labels=["label"],
+    # )
+    sampler = ShaDowKHopSampler(
+        [10, 5],
         prefetch_node_feats=["feat"],
-        prefetch_labels=["label"],
     )
     train_dataloader = DataLoader(
         g,
@@ -309,7 +269,7 @@ def train(rank, world_size, args, g, data):
         use_uva=not is_cpu_proc(args.cpu_process),
         drop_last=drop_last,
         shuffle=shuffle,
-        num_workers=4 if is_cpu_proc(args.cpu_process) else 0,
+        num_workers=8 if is_cpu_proc(args.cpu_process) else 0,
     )
 
     # training loop
@@ -342,16 +302,82 @@ def train(rank, world_size, args, g, data):
             else:
                 loss = _train(**params)
             if rank == 0: print(f'Epoch Time: {time.time() - tik:.4f}')
-        if epoch == 1 and rank == args.cpu_process:
-            prof.export_chrome_trace('mixture_product.json')
+        if epoch == 1:
+            prof.export_chrome_trace(TRACE_NAME.format(rank))
 
         dist.barrier()
 
         # TODO: val or test
         if rank == 0:
-            print(f'Training loss: {loss/train_indices.num_batches:.4f}')
+            print(f'Training loss: {loss / train_indices.num_batches:.4f}')
 
     dist.destroy_process_group()
+
+
+def train_single_gpu(rank, world_size, args, g, data):
+    # delete all unnecessary ddp wrappers
+    num_classes, train_idx, val_idx, test_idx = data
+    device = torch.device("cuda:0")
+    torch.cuda.set_device(device)
+
+    # create GraphSAGE model
+    in_size = g.ndata["feat"].shape[1]
+    model = SAGE(in_size, 128, num_classes).to(device)
+
+    # create loader
+    # sampler = NeighborSampler(
+    #     [15, 10, 5],
+    #     prefetch_node_feats=["feat"],
+    #     prefetch_labels=["label"],
+    # )
+    sampler = ShaDowKHopSampler(
+        [10, 5],
+        # prefetch_node_feats=["feat"],
+    )
+    train_idx = test_idx
+    train_dataloader = DataLoader(
+        g,
+        train_idx.to(device),
+        sampler,
+        device=device,
+        batch_size=args.batch_size,
+        use_uva=True,
+        drop_last=True,
+        shuffle=True,
+        num_workers=0,
+    )
+
+    # training loop
+    opt = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=5e-4)
+    params = {
+        # training
+        'loader': train_dataloader,
+        'model': model,
+        'opt': opt,
+        # logging
+        'rank': rank,
+        'train_size': len(train_idx),
+        'batch_size': args.batch_size,
+    }
+    for epoch in range(2):
+        params['epoch'] = epoch
+        model.train()
+        with profile(
+                activities=[
+                    ProfilerActivity.CPU,
+                    ProfilerActivity.CUDA
+                ],
+                record_shapes=True
+        ) if True else nullcontext() as prof:
+            tik = time.time()
+            loss = _train(**params)
+            if rank == 0: print(f'Epoch Time: {time.time() - tik:.4f}')
+        if epoch == 1:
+            prof.export_chrome_trace(TRACE_NAME.format(rank))
+
+        # TODO: val or test
+        if rank == 0:
+            print(f'Training loss: {loss / len(train_idx):.4f}')
 
 
 if __name__ == "__main__":
@@ -372,7 +398,7 @@ if __name__ == "__main__":
                         default=0.3)
     parser.add_argument("--batch_size",
                         type=int,
-                        default=4096)
+                        default=8192)
     arguments = parser.parse_args()
 
     # Assure Consistency
@@ -413,9 +439,15 @@ if __name__ == "__main__":
     processes = []
     for i in range(nprocs):
         p = dmp.Process(target=train, args=(i, nprocs, arguments, g, data))
+        # p = dmp.Process(target=train_single_gpu, args=(i, nprocs, arguments, g, data))
         p.start()
         processes.append(p)
     for p in processes:
         p.join()
+
+    input_files = [TRACE_NAME.format(i) for i in range(nprocs)]
+    merge_trace_files(input_files, OUTPUT_TRACE_NAME)
+    for i in range(nprocs):
+        os.remove(TRACE_NAME.format(i))
 
     print("Program finished")
